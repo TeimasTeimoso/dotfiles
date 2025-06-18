25cd3dc4591ce4392a707607eed1ce0c668c52f5
TeimasTeimoso <ruben.teimas@uevora.pt>
2025-06-19 00:06:19 +0100

    removed flags from rofi

--- configs/waybar/scripts/power-menu.sh
+++ configs/waybar/scripts/power-menu.sh
@@ -5,7 +5,7 @@ config="$HOME/.config/rofi/power-menu.rasi"
 actions=$(echo -e "  Lock\n  Reboot\n Shutdown\n Logout")
 
 # Display logout menu
-selected_option=$(echo -e "$actions" | rofi -dmenu -i -config "${config}" -no-lazy-grab -normal-window || pkill -x rofi)
+selected_option=$(echo -e "$actions" | rofi -dmenu -i -config "${config}" || pkill -x rofi)
 
 # Perform actions based on the selected option
 case "$selected_option" in
